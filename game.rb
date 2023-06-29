46f5ab84a6d815b6e13c43b0826b2f0ff9c18219
Adzi <belkoadam@gmail.com>
2023-06-29 17:13:46 +1000

    Taking input from user in place

--- game.rb
+++ game.rb
@@ -5,6 +5,26 @@ class Game
 
   def play
     display_instructions
+    game_mode = take_input
+
+    code_maker if game_mode == "1"
+    code_braker if game_mode == "2"
+  end
+
+  def take_input
+    input = gets.chomp
+    return input if %w[1 2].include?(input)
+
+    puts "Incorrect input"
+    take_input
+  end
+
+  def code_maker
+    puts "Code maker"
+  end
+
+  def code_braker
+    puts "Code braker"
   end
 end
 
